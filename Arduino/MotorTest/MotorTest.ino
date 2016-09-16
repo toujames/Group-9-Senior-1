9e602d6c8edd1f0c5d2323d6ada9fd32d17080eb
James Touthang <james@touthang.info>
2016-09-15 19:50:58 -0500

    added the method orientation

--- Arduino/MotorTest/MotorTest.ino
+++ Arduino/MotorTest/MotorTest.ino
@@ -2,19 +2,20 @@
 
 Servo myservo;
 
-void arm(){
-  // arm the speed controller, modify as necessary for your ESC 
-  
-  setSpeed(0); 
-  delay(2000); //delay 1 second, some speed controllers may need longer
-}
-
 void setSpeed(int speed){
   // speed is from 54 to 100, where 53 is off and 100 is maximum
   // 54 speed is the slowest. Found form testing
   // the following maps speed values of 0-100 to angles from 0-180,
   int angle = map(speed, 0, 100, 0, 180);
+
+  // New Mapping for Motor Control
   myservo.write(angle); 
+}
+
+void arm(){
+  // arm the speed controller, modify as necessary for your ESC 
+  setSpeed(0); 
+  delay(2000); //delay 1 second, some speed controllers may need longer
 }
 
 void setup()
@@ -22,6 +23,7 @@ void setup()
   myservo.attach(9);
   arm(); 
 }
+
 void loop()
 {
   setSpeed(54); 
